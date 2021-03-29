cffc8200a1329533a2218ea89186fc94bc1cbef4
tomsk-kozlov-ilya@mail.ru <tomsk-kozlov-ilya@mail.ru>
2021-03-30 00:41:16 +0700

    Добавьте файлы проекта.

---
+++ Calculator/CalculatorModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorModel
+    {
+        public CalculatorModel()
+        {
+            
+        }
+
+        public object Calculate(int operand1, int operand2, char operation)
+        {
+            switch (operation)
+            {
+                case '+':
+                    return operand1 + operand2;
+                case '-':
+                    return operand1 - operand2;
+                case '*':
+                    return operand1 * operand2;
+                case '/':
+                    return operand1 / (double)operand2;
+                default:
+                    return null;
+            };
+        }
+
+        
+    }
+}
