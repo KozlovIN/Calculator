814b0cfaafd846ff7e3cf44cfcddc59d0e709fe9
tomsk-kozlov-ilya@mail.ru <tomsk-kozlov-ilya@mail.ru>
2021-03-30 00:50:49 +0700

    Calculator2and3

---
+++ Calculator2/CalculatorModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorModel
+    {
+        public CalculatorModel()
+        {
+
+        }
+
+        public object Calculate(int operand1, int operand2, char operation)
+        {
+
+            switch (operation)
+            {
+                case '+':
+                    return Sum(operand1, operand2);
+                case '-':
+                    return Difference(operand1, operand2);
+                case '*':
+                    return Multiplication(operand1, operand2);
+                case '/':
+                    return Division(operand1, operand2);
+                default:
+                    return null;
+            }
+
+
+
+
+        }
+        static private int Sum(int operand1, int operand2)
+          {
+            int sum = operand1 + operand2;
+            return sum;
+          }
+
+        static private int Difference(int operand1, int operand2)
+          {
+            int dif = operand1 - operand2;
+            return dif;
+          }
+        static private int Multiplication(int operand1, int operand2)
+          {
+            int mul = operand1 * operand2;
+            return mul;
+          }
+        static private double Division(int operand1, int operand2)
+        {
+            double div = (double)operand1 / (double)operand2;
+            return div;
+        }
+    }
+}
---
+++ Calculator3/CalculatorModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class CalculatorModel
+    {
+        public CalculatorModel()
+        {
+
+        }
+
+        public object Calculate(int operand1, int operand2, char operation)
+        {
+
+            if (operation == '+')
+            {
+                return Sum(operand1, operand2);
+            }
+            else if (operation == '-')
+            {
+                return Difference(operand1, operand2);
+            }
+            else if (operation == '*')
+            {
+                return Multiplication(operand1, operand2);
+            }
+            else if (operation == '/')
+            {
+                return Division(operand1, operand2);
+            }
+            else
+                    return null;
+
+
+        }
+        static private int Sum(int operand1, int operand2)
+          {
+            int sum = operand1 + operand2;
+            return sum;
+          }
+
+        static private int Difference(int operand1, int operand2)
+          {
+            int dif = operand1 - operand2;
+            return dif;
+          }
+        static private int Multiplication(int operand1, int operand2)
+          {
+            int mul = operand1 * operand2;
+            return mul;
+          }
+        static private double Division(int operand1, int operand2)
+        {
+            double div = (double)operand1 / (double)operand2;
+            return div;
+        }
+    }
+}
